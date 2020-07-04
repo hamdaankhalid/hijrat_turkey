bcddede48944839bd6890682e1348edacc951462
Hamdaan khalid <hamdaankhalid@Hamdaans-MacBook.local>
2020-07-04 15:51:28 -0500

    removed venv from wrong folder

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/hamdaankhalid/Desktop/Harvard Web Dev/Real Estate/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
